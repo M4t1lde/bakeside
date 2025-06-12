85ffb535fe04a8fc9efca5eee31f0199c821d02f
Matilde <matildejb@outlook.com>
2025-06-12 11:58:20 +0200

    kms bruker endringer

--- dbconnector.js
+++ dbconnector.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: 'localhost', //ip-en til ubuntu vm 172.31.1.72
+  host: '172.31.1.72', //ip-en til ubuntu vm 172.31.1.72
   user: 'matilde',
   password: '1487',
   database: 'bakeside',
